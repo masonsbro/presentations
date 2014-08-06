1ab57f46ee4e296f137028b6b1e354f2a25c607d
Ethan Arnold <masonsbro456@gmail.com>
2014-08-05 22:34:14 -0500

    Add to INSTALLED_APPS

--- presentations/settings.py
+++ presentations/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'south',
+    'presentationsapp',
 )
 
 MIDDLEWARE_CLASSES = (
