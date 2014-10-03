0b3220b0e212bba1dd197e6d2304249142b332c8
Ethan Arnold <masonsbro456@gmail.com>
2014-10-02 19:41:05 -0500

    Add save for registration & login logic

--- presentationsapp/views.py
+++ presentationsapp/views.py
@@ -8,6 +8,14 @@ def index(req):
 	return render(req, "landing.html", {'request': req})
 
 def login(req):
+	if req.method == 'POST':
+		try:
+			user = User.objects.get(email = req.POST['email'])
+			if user.check_password(req.POST['password']):
+				req.session['email'] = req.POST['email']
+				return redirect("/")
+		except:
+			pass
 	return render(req, "login.html", {'request': req})
 
 def register(req):
@@ -15,6 +23,7 @@ def register(req):
 		try:
 			user = User(email = req.POST['email'])
 			user.set_password(req.POST['password'])
+			user.save()
 			req.session['email'] = req.POST['email']
 			return redirect("/")
 		except:
